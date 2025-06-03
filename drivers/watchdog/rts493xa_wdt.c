0aaf0a6126ef44ba03c426d2d5e4773e68769db6
todd_jin <todd_jin@realsil.com.cn>
2025-06-03 14:34:40 +0800

    refactor: using kernel wdt framework to refactor rts493xa wdt driver
    Change-Id: Ic6eb91f8c246d36d53cbae91af65f7b2c33f1d77
    Signed-off-by: todd_jin <todd_jin@realsil.com.cn>

--- drivers/watchdog/rts493xa_wdt.c
+++ drivers/watchdog/rts493xa_wdt.c
@@ -21,24 +21,16 @@
  * even if advised of the possibility of such damage.
  */
 
-#include <linux/module.h> /* For module specific items */
-#include <linux/moduleparam.h> /* For new moduleparam's */
-#include <linux/types.h> /* For standard types (like size_t) */
-#include <linux/errno.h> /* For the -ENODEV/... values */
-#include <linux/kernel.h> /* For printk/panic/... */
-#include <linux/notifier.h>
-#include <linux/reboot.h>
+#include <linux/bitops.h>
 #include <linux/delay.h>
-#include <linux/fs.h> /* For file operations */
-#include <linux/miscdevice.h>
-#include <linux/watchdog.h> /* For the watchdog specific items */
-#include <linux/init.h> /* For __init/__exit/... */
-#include <linux/platform_device.h> /* For platform_driver framework */
-#include <linux/spinlock.h> /* For spin_lock/spin_unlock/... */
-#include <linux/uaccess.h> /* For copy_to_user/put_user/... */
-#include <linux/of.h>
-#include <linux/of_device.h>
 #include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/reset.h>
+#include <linux/units.h>
+#include <linux/watchdog.h>
 
 #define DRIVER_NAME "rts493xa-wdt"
 
@@ -58,31 +50,29 @@
 #define WDOG_RST_EN		 1
 #define WDOG_EN			 0
 
-#define WATCHDOG_TIMEOUT 8
+#define WDT_DEFAULT_TIMEOUT 8
 
 #define RTS_GETFIELD(val, width, offset) ((val >> offset) & ((1 << width) - 1))
 #define RTS_SETFIELD(reg, field, width, offset)      \
 	((reg & (~(((1 << width) - 1) << offset))) | \
 	 ((field & ((1 << width) - 1)) << offset))
 
-static struct {
-	unsigned long inuse;
-	spinlock_t io_lock;
-} rts_wdt_device;
-
-static void __iomem *wdt_reg;
-static int expect_close;
-static int timeout = WATCHDOG_TIMEOUT;
-static bool nowayout = WATCHDOG_NOWAYOUT;
-static int ictype;
-
-enum {
+enum rts_wdt_type {
 	TYPE_RTS493XA = 1,
 
 	TYPE_FPGA = (1 << 16),
 };
 
-#define RTS_SOC_HW_ID(type) ((type) & 0xff)
+struct rts_wdt_priv {
+	struct watchdog_device wdd;
+	void __iomem *wdt_reg;
+	struct clk *clk;
+	enum rts_wdt_type devtype;
+};
+
+static bool nowayout = WATCHDOG_NOWAYOUT;
+module_param(nowayout, bool, 0);
+MODULE_PARM_DESC(nowayout, "Disable watchdog shutdown on close");
 
 static void rts_set_field(void __iomem *reg, unsigned int field,
 			  unsigned int width, unsigned int offset)
@@ -93,301 +83,152 @@ static void rts_set_field(void __iomem *reg, unsigned int field,
 	writel(val, reg);
 }
 
-static int rts_wdt_set(int new_timeout)
+static int rts_wdt_start(struct watchdog_device *wdd)
 {
-	u8 time;
-	unsigned long flag;
-
-	spin_lock_irqsave(&rts_wdt_device.io_lock, flag);
-
-	if (new_timeout >= 64) {
-		timeout = 64;
-		time = 6;
-	} else if (new_timeout >= 32) {
-		timeout = 32;
-		time = 5;
-	} else if (new_timeout >= 16) {
-		timeout = 16;
-		time = 4;
-	} else if (new_timeout >= 8) {
-		timeout = 8;
-		time = 3;
-	} else if (new_timeout >= 4) {
-		timeout = 4;
-		time = 2;
-	} else if (new_timeout >= 2) {
-		timeout = 2;
-		time = 1;
-	} else {
-		timeout = 1;
-		time = 0;
-	}
-
-	rts_set_field(wdt_reg + WATCHDOG_CFG_REG, time, 3, WDOG_TIME_2);
-
-	spin_unlock_irqrestore(&rts_wdt_device.io_lock, flag);
-
-	return 0;
-}
-
-static void rts_wdt_start(void)
-{
-	unsigned long flag;
-
-	rts_wdt_set(timeout);
-
-	spin_lock_irqsave(&rts_wdt_device.io_lock, flag);
+	struct rts_wdt_priv *priv = watchdog_get_drvdata(wdd);
 
 #ifdef CONFIG_EXTERNAL_RESET
 	rts_set_field(wdt_reg + WATCHDOG_CFG_REG, 1, 1, WDOG_RST_PMU_ENABLE);
 #else
-	rts_set_field(wdt_reg + WATCHDOG_CFG_REG, 1, 1, WDOG_RST_EN);
+	rts_set_field(priv->wdt_reg + WATCHDOG_CFG_REG, 1, 1, WDOG_RST_EN);
 #endif
-	rts_set_field(wdt_reg + WATCHDOG_CFG_REG, 1, 1, WDOG_EN);
+	rts_set_field(priv->wdt_reg + WATCHDOG_CFG_REG, 1, 1, WDOG_EN);
 
-	spin_unlock_irqrestore(&rts_wdt_device.io_lock, flag);
 	pr_info("Started watchdog timer\n");
+
+	return 0;
 }
 
-static void rts_wdt_stop(void)
+static int rts_wdt_stop(struct watchdog_device *wdd)
 {
-	unsigned long flag;
+	struct rts_wdt_priv *priv = watchdog_get_drvdata(wdd);
 
-	spin_lock_irqsave(&rts_wdt_device.io_lock, flag);
+	rts_set_field(priv->wdt_reg + WATCHDOG_CFG_REG, 0, 1, WDOG_EN);
 
-	rts_set_field(wdt_reg + WATCHDOG_CFG_REG, 0, 1, WDOG_EN);
-
-	spin_unlock_irqrestore(&rts_wdt_device.io_lock, flag);
 	pr_info("Stopped watchdog timer\n");
-}
 
-static void rts_wdt_ping(void)
-{
-	unsigned long flag;
-
-	spin_lock_irqsave(&rts_wdt_device.io_lock, flag);
-	rts_set_field(wdt_reg + WATCHDOG_CTL, 1, 1, 0);
-	spin_unlock_irqrestore(&rts_wdt_device.io_lock, flag);
-}
-
-static int rts_wdt_open(struct inode *inode, struct file *file)
-{
-	if (test_and_set_bit(0, &rts_wdt_device.inuse))
-		return -EBUSY;
-
-	if (nowayout)
-		__module_get(THIS_MODULE);
-
-	rts_wdt_start();
-	rts_wdt_ping();
-
-	return nonseekable_open(inode, file);
-}
-
-static int rts_wdt_release(struct inode *inode, struct file *file)
-{
-	if (expect_close == 42) {
-		rts_wdt_stop();
-		module_put(THIS_MODULE);
-	} else {
-		pr_crit("device closed\n");
-		rts_wdt_ping();
-	}
-	clear_bit(0, &rts_wdt_device.inuse);
 	return 0;
 }
 
-static ssize_t rts_wdt_write(struct file *file, const char *data, size_t len,
-			     loff_t *ppos)
+static int rts_wdt_ping(struct watchdog_device *wdd)
 {
-	if (len) {
-		if (!nowayout) {
-			size_t i;
+	struct rts_wdt_priv *priv = watchdog_get_drvdata(wdd);
 
-			expect_close = 0;
+	rts_set_field(priv->wdt_reg + WATCHDOG_CTL, 1, 1, 0);
 
-			for (i = 0; i != len; i++) {
-				char c;
-
-				if (get_user(c, data + i))
-					return -EFAULT;
-				if (c == 'V')
-					expect_close = 42;
-			}
-		}
-		rts_wdt_ping();
-		return len;
-	}
 	return 0;
 }
 
-static int wdt_restart_handle(struct notifier_block *this, unsigned long mode,
-			      void *cmd)
+static int rts_wdt_set_timeout(struct watchdog_device *wdd,
+			       unsigned int timeout)
 {
-	rts_wdt_set(1);
-	rts_wdt_start();
+	struct rts_wdt_priv *priv = watchdog_get_drvdata(wdd);
+	u8 time;
+
+	if (timeout >= 64)
+		time = 6;
+	else if (timeout >= 32)
+		time = 5;
+	else if (timeout >= 16)
+		time = 4;
+	else if (timeout >= 8)
+		time = 3;
+	else if (timeout >= 4)
+		time = 2;
+	else if (timeout >= 2)
+		time = 1;
+	else
+		time = 0;
+
+	wdd->timeout = timeout;
+	wdd->max_hw_heartbeat_ms = (1 << time) * 1000 / 2;
+
+	rts_set_field(priv->wdt_reg + WATCHDOG_CFG_REG, time, 3, WDOG_TIME_2);
+
+	return 0;
+}
+
+static int rts_wdt_restart(struct watchdog_device *wdd, unsigned long action,
+			   void *data)
+{
+	rts_wdt_set_timeout(wdd, 1);
+	rts_wdt_start(wdd);
 
 	mdelay(2000);
 
 	pr_emerg("Unable to restart system\n");
 
-	return NOTIFY_DONE;
+	return 0;
 }
 
-static struct notifier_block wdt_restart_handler = {
-	.notifier_call = wdt_restart_handle,
-	.priority = 128,
+static const struct watchdog_info rts_wdt_ident = {
+	.options = WDIOF_MAGICCLOSE | WDIOF_KEEPALIVEPING | WDIOF_SETTIMEOUT,
+	.identity = "RTS_WDT Watchdog",
 };
 
-static long rts_wdt_ioctl(struct file *file, unsigned int cmd,
-			  unsigned long arg)
+static const struct watchdog_ops rts_wdt_ops = {
+	.owner = THIS_MODULE,
+	.start = rts_wdt_start,
+	.stop = rts_wdt_stop,
+	.ping = rts_wdt_ping,
+	.set_timeout = rts_wdt_set_timeout,
+	.restart = rts_wdt_restart,
+};
+
+static int rts_wdt_probe(struct platform_device *pdev)
 {
-	void __user *argp = (void __user *)arg;
-	int new_timeout;
-	unsigned int value;
-	static const struct watchdog_info ident = {
-		.options = WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING |
-			   WDIOF_MAGICCLOSE,
-		.identity = "RTS_WDT Watchdog",
-	};
-	switch (cmd) {
-	case WDIOC_GETSUPPORT:
-		if (copy_to_user(argp, &ident, sizeof(ident)))
-			return -EFAULT;
-		break;
-	case WDIOC_GETSTATUS:
-	case WDIOC_GETBOOTSTATUS:
-		value = 0;
-		if (copy_to_user(argp, &value, sizeof(int)))
-			return -EFAULT;
-		break;
-	case WDIOC_SETOPTIONS:
-		if (copy_from_user(&value, argp, sizeof(int)))
-			return -EFAULT;
-		switch (value) {
-		case WDIOS_ENABLECARD:
-			rts_wdt_start();
-			break;
-		case WDIOS_DISABLECARD:
-			rts_wdt_stop();
-			break;
-		default:
-			return -EINVAL;
-		}
-		break;
-	case WDIOC_KEEPALIVE:
-		rts_wdt_ping();
-		break;
-	case WDIOC_SETTIMEOUT:
-		if (copy_from_user(&new_timeout, argp, sizeof(int)))
-			return -EFAULT;
-		if (rts_wdt_set(new_timeout))
-			return -EINVAL;
-		break;
-	case WDIOC_GETTIMEOUT:
-		return copy_to_user(argp, &timeout, sizeof(int));
-	default:
-		return -ENOTTY;
-	}
+	struct device *dev = &pdev->dev;
+	struct rts_wdt_priv *priv;
+	int ret;
+
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->wdt_reg = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(priv->wdt_reg))
+		return PTR_ERR(priv->wdt_reg);
+
+	priv->devtype = (uintptr_t)of_device_get_match_data(dev);
+
+	priv->wdd.info = &rts_wdt_ident;
+	priv->wdd.ops = &rts_wdt_ops;
+	priv->wdd.parent = dev;
+	priv->wdd.min_timeout = 1;
+	priv->wdd.max_timeout = 64;
+	priv->wdd.max_hw_heartbeat_ms = 64 * 1000;
+	priv->wdd.timeout = WDT_DEFAULT_TIMEOUT;
+
+	watchdog_set_drvdata(&priv->wdd, priv);
+	watchdog_set_nowayout(&priv->wdd, nowayout);
+	watchdog_stop_on_unregister(&priv->wdd);
+
+	rts_wdt_stop(&priv->wdd);
+	rts_wdt_set_timeout(&priv->wdd, WDT_DEFAULT_TIMEOUT);
+
+	ret = devm_watchdog_register_device(dev, &priv->wdd);
+	if (ret)
+		return ret;
+
+	platform_set_drvdata(pdev, priv);
 
 	return 0;
 }
 
-static const struct file_operations rts_wdt_fops = {
-	.owner = THIS_MODULE,
-	.llseek = no_llseek,
-	.write = rts_wdt_write,
-	.unlocked_ioctl = rts_wdt_ioctl,
-	.open = rts_wdt_open,
-	.release = rts_wdt_release,
-};
-
-static struct miscdevice rts_wdt_miscdev = {
-	.minor = WATCHDOG_MINOR,
-	.name = "watchdog",
-	.fops = &rts_wdt_fops,
-};
-
-static const struct of_device_id rts493xa_wd_match[] = {
+static const struct of_device_id rts493xa_wdt_match[] = {
 	{
 		.compatible = "realtek,rts493xa-wdt",
 		.data = (void *)(TYPE_RTS493XA),
 	},
 	{}
 };
-MODULE_DEVICE_TABLE(of, rts493xa_wd_match);
-
-static int rts_wdt_probe(struct platform_device *pdev)
-{
-	int ret;
-	struct resource *r;
-	const struct of_device_id *of_id;
-
-	of_id = of_match_device(rts493xa_wd_match, &pdev->dev);
-
-	ictype = RTS_SOC_HW_ID((int)of_id->data);
-
-	r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!r) {
-		pr_err("failed to retrieve resources\n");
-		return -ENODEV;
-	}
-
-	wdt_reg = ioremap(r->start, resource_size(r));
-	if (!wdt_reg) {
-		pr_err("failed to remap I/O resources\n");
-		return -ENXIO;
-	}
-
-	spin_lock_init(&rts_wdt_device.io_lock);
-
-	rts_wdt_stop();
-
-	if (rts_wdt_set(timeout))
-		rts_wdt_set(WATCHDOG_TIMEOUT);
-
-	ret = register_restart_handler(&wdt_restart_handler);
-	if (ret) {
-		pr_err("cannot register restart handler (err=%d)\n", ret);
-		goto err_out_reboot;
-	}
-
-	ret = misc_register(&rts_wdt_miscdev);
-	if (ret < 0) {
-		pr_err("failed to register watchdog device\n");
-		goto unmap;
-	}
-
-	pr_info("timer margin: %d sec\n", timeout);
-
-	return 0;
-err_out_reboot:
-	unregister_restart_handler(&wdt_restart_handler);
-unmap:
-	iounmap(wdt_reg);
-	return ret;
-}
-
-static int rts_wdt_remove(struct platform_device *pdev)
-{
-	misc_deregister(&rts_wdt_miscdev);
-	unregister_restart_handler(&wdt_restart_handler);
-	iounmap(wdt_reg);
-	return 0;
-}
-
-static void rts_wdt_shutdown(struct platform_device *pdev)
-{
-	rts_wdt_stop();
-}
+MODULE_DEVICE_TABLE(of, rts493xa_wdt_match);
 
 static struct platform_driver rts_wdt_driver = {
 	.probe = rts_wdt_probe,
-	.remove = rts_wdt_remove,
-	.shutdown = rts_wdt_shutdown,
 	.driver = {
 		.name = "watchdog-platform",
-		.of_match_table = rts493xa_wd_match,
+		.of_match_table = rts493xa_wdt_match,
 	},
 };
 module_platform_driver(rts_wdt_driver);
