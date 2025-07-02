c2c9e1daad2efc28fec1a6ef899e292fb9451262
todd_jin <todd_jin@realsil.com.cn>
2025-07-02 14:56:59 +0800

    refactor: use ddrc as thermal cooling device
    Change-Id: Ib63db2a5f5b05ee4f25371815bcaada4773f082a
    Signed-off-by: todd_jin <todd_jin@realsil.com.cn>

--- drivers/misc/rts493x-ddrc.c
+++ drivers/misc/rts493x-ddrc.c
@@ -32,7 +32,7 @@
 #include <linux/slab.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/cdev.h>
-#include "rts493x-ddrc.h"
+#include <linux/thermal.h>
 
 /* DDR controller register offsets */
 #define DRR 0x0010
@@ -47,11 +47,17 @@
 
 #define REF_PLL 25000000
 
+enum {
+	DDR_STATUS_NORMAL = 0,
+	DDR_STATUS_HOT,
+};
+
 struct rts_ddrc {
 	struct device *dev;
-	void __iomem *cregs; /*ddr controller register*/
-	void __iomem *pregs; /*ddr phy register*/
+	void __iomem *cregs;
+	void __iomem *pregs;
 	u32 tref;
+	int cur_state;
 };
 
 static ssize_t ddrc_tref_show(struct device *_dev,
@@ -78,80 +84,50 @@ static const struct attribute_group ddrc_attr_group = {
 	.attrs = (struct attribute **)ddrc_attrs,
 };
 
-static inline u32 rts_readl_c(struct rts_ddrc *rddr, u32 reg)
+static inline u32 rts_readl_c(struct rts_ddrc *rddrc, u32 reg)
 {
-	return readl(rddr->cregs + reg);
+	return readl(rddrc->cregs + reg);
 }
 
-static inline void rts_writel_c(struct rts_ddrc *rddr, u32 reg, u32 val)
+static inline void rts_writel_c(struct rts_ddrc *rddrc, u32 reg, u32 val)
 {
-	writel(val, rddr->cregs + reg);
+	writel(val, rddrc->cregs + reg);
 }
 
-static inline u32 rts_readl_p(struct rts_ddrc *rddr, u32 reg)
+static inline u32 rts_readl_p(struct rts_ddrc *rddrc, u32 reg)
 {
-	return readl(rddr->pregs + reg);
+	return readl(rddrc->pregs + reg);
 }
 
-static inline void rts_writel_p(struct rts_ddrc *rddr, u32 reg, u32 val)
+static inline void rts_writel_p(struct rts_ddrc *rddrc, u32 reg, u32 val)
 {
-	writel(val, rddr->pregs + reg);
+	writel(val, rddrc->pregs + reg);
 }
 
-static u64 integer_div(u64 dividend, u64 divisor)
-{
-	u64 res;
-
-	res = 0;
-
-	if (divisor == 0) {
-		pr_err("wrong divisor\n");
-		return 0;
-	}
-
-	if (dividend < divisor)
-		return 0;
-
-	while (dividend > divisor) {
-		dividend -= divisor;
-		res++;
-	}
-
-	if (dividend == divisor)
-		res++;
-
-	return res;
-}
-
-static int get_dram_clock(struct rts_ddrc *rddr)
+static int get_dram_clock(struct rts_ddrc *rddrc)
 {
 	u64 dpi_n_code, dpi_f_code;
 	u64 clock;
 
-	dpi_f_code = rts_readl_p(rddr, SSC2) & 0x7ff;
-	dpi_n_code = rts_readl_p(rddr, SSC3) & 0xff;
+	dpi_f_code = rts_readl_p(rddrc, SSC2) & 0x7ff;
+	dpi_n_code = rts_readl_p(rddrc, SSC3) & 0xff;
 
-	clock = integer_div(REF_PLL * dpi_f_code * 2, 2048);
+	clock = div_u64(REF_PLL * dpi_f_code * 2, 2048);
 
-	clock = integer_div((clock + REF_PLL * (dpi_n_code + 3) * 2 + 500000),
-			    1000000);
+	clock = div_u64((clock + REF_PLL * (dpi_n_code + 3) * 2 + 500000),
+			1000000);
 
 	return (int)clock;
 }
 
-static struct rts_ddrc *_rts_ddrc;
-
-static int ddrc_notifier(struct notifier_block *nb, unsigned long event,
-			 void *data)
+static int rts_ddrc_adjust_tref(struct rts_ddrc *rddrc, int state)
 {
-	int retval = 0;
 	int ddr_clock;
 	int pctl_period_ps = 2500;
 	u32 drr_val_temp;
 	u32 drr_tref_val_temp;
 
-	/*get ddr clock*/
-	ddr_clock = get_dram_clock(_rts_ddrc);
+	ddr_clock = get_dram_clock(rddrc);
 
 	switch (ddr_clock) {
 	case 800:
@@ -173,45 +149,73 @@ static int ddrc_notifier(struct notifier_block *nb, unsigned long event,
 		pctl_period_ps = 1875;
 		break;
 	default:
-		retval = -EINVAL;
-		break;
+		dev_err(rddrc->dev, "invalid ddrc clock %d\n", ddr_clock);
+		return -EINVAL;
 	}
 
-	drr_val_temp = rts_readl_c(_rts_ddrc, DRR);
+	drr_val_temp = rts_readl_c(rddrc, DRR);
 	drr_tref_val_temp = ((u16)(drr_val_temp >> 8) + 0x100) * pctl_period_ps;
 
-	_rts_ddrc->tref = drr_tref_val_temp;
+	rddrc->tref = drr_tref_val_temp;
 
-	/*low event and current value is high, need to change*/
-	if ((event == 0) && (_rts_ddrc->tref < 59900000)) {
-		dev_info(_rts_ddrc->dev,
-			 "low event and current value is high\n");
-		_rts_ddrc->tref = 60000000;
-		drr_val_temp =
-			(drr_val_temp & 0xff0000ff) |
-			((_rts_ddrc->tref / pctl_period_ps - 0x100) << 8);
-		rts_writel_c(_rts_ddrc, DRR, drr_val_temp);
-
-		return retval;
-	}
-	/*high event and current value is low, need to change*/
-	if ((event == 1) && (_rts_ddrc->tref > 59900000)) {
-		dev_info(_rts_ddrc->dev,
-			 "high event and current value is low\n");
-		_rts_ddrc->tref = 30000000;
-		drr_val_temp =
-			(drr_val_temp & 0xff0000ff) |
-			((_rts_ddrc->tref / pctl_period_ps - 0x100) << 8);
-		rts_writel_c(_rts_ddrc, DRR, drr_val_temp);
-
-		return retval;
+	if ((state == DDR_STATUS_NORMAL) && (rddrc->tref < 59900000)) {
+		dev_info(rddrc->dev, "switch to DDR normal state parameters\n");
+		rddrc->tref = 60000000;
+		drr_val_temp = (drr_val_temp & 0xff0000ff) |
+			       ((rddrc->tref / pctl_period_ps - 0x100) << 8);
+		rts_writel_c(rddrc, DRR, drr_val_temp);
 	}
 
-	return retval;
+	if ((state == DDR_STATUS_HOT) && (rddrc->tref > 59900000)) {
+		dev_info(rddrc->dev, "switch to DDR hot state parameters\n");
+		rddrc->tref = 30000000;
+		drr_val_temp = (drr_val_temp & 0xff0000ff) |
+			       ((rddrc->tref / pctl_period_ps - 0x100) << 8);
+		rts_writel_c(rddrc, DRR, drr_val_temp);
+	}
+
+	return 0;
 }
 
-static struct notifier_block ddrc_nb = {
-	.notifier_call = ddrc_notifier,
+static int ddrc_get_max_state(struct thermal_cooling_device *cdev,
+			      unsigned long *state)
+{
+	*state = DDR_STATUS_HOT;
+
+	return 0;
+}
+
+static int ddrc_get_cur_state(struct thermal_cooling_device *cdev,
+			      unsigned long *state)
+{
+	struct rts_ddrc *rddrc = cdev->devdata;
+
+	*state = rddrc->cur_state;
+
+	return 0;
+}
+
+static int ddrc_set_cur_state(struct thermal_cooling_device *cdev,
+			      unsigned long state)
+{
+	struct rts_ddrc *rddrc = cdev->devdata;
+	int ret;
+
+	ret = rts_ddrc_adjust_tref(rddrc, state);
+	if (ret) {
+		dev_err(rddrc->dev, "failed to adjust tref\n");
+		return ret;
+	}
+
+	rddrc->cur_state = state;
+
+	return 0;
+}
+
+static struct thermal_cooling_device_ops ddrc_cooling_ops = {
+	.get_max_state = ddrc_get_max_state,
+	.get_cur_state = ddrc_get_cur_state,
+	.set_cur_state = ddrc_set_cur_state,
 };
 
 static const struct of_device_id rts_ddrc_dt_ids[] = {
@@ -225,59 +229,52 @@ MODULE_DEVICE_TABLE(of, rts_ddrc_dt_ids);
 
 static int rts_ddrc_probe(struct platform_device *pdev)
 {
-	struct rts_ddrc *rddr;
-	struct resource *res;
+	struct rts_ddrc *rddrc;
+	struct thermal_cooling_device *cdev;
 	int err;
 
-	rddr = devm_kzalloc(&pdev->dev, sizeof(*rddr), GFP_KERNEL);
-	if (!rddr)
+	rddrc = devm_kzalloc(&pdev->dev, sizeof(*rddrc), GFP_KERNEL);
+	if (!rddrc)
 		return -ENOMEM;
 
-	rddr->dev = &pdev->dev;
+	rddrc->dev = &pdev->dev;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-	if (!res) {
-		dev_err(&pdev->dev, "no memory resource provided");
-		return -ENXIO;
+	rddrc->cregs = devm_platform_get_and_ioremap_resource(pdev, 0, NULL);
+	if (IS_ERR(rddrc->cregs)) {
+		dev_err(&pdev->dev, "get and ioremap memory resource 0 failed");
+		return PTR_ERR(rddrc->cregs);
 	}
 
-	rddr->cregs = devm_ioremap_resource(&pdev->dev, res);
-
-	if (IS_ERR(rddr->cregs))
-		return PTR_ERR(rddr->cregs);
-
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
-	if (!res) {
+	rddrc->pregs = devm_platform_get_and_ioremap_resource(pdev, 1, NULL);
+	if (IS_ERR(rddrc->pregs)) {
 		dev_err(&pdev->dev, "no memory resource provided");
-		return -ENXIO;
+		return PTR_ERR(rddrc->pregs);
 	}
 
-	rddr->pregs = devm_ioremap_resource(&pdev->dev, res);
-	if (IS_ERR(rddr->pregs))
-		return PTR_ERR(rddr->pregs);
+	platform_set_drvdata(pdev, rddrc);
 
-	platform_set_drvdata(pdev, rddr);
-
-	err = sysfs_create_group(&rddr->dev->kobj, &ddrc_attr_group);
+	err = sysfs_create_group(&rddrc->dev->kobj, &ddrc_attr_group);
 	if (err < 0)
 		return err;
 
-	thermal_event_register(&ddrc_nb);
+	cdev = devm_thermal_of_cooling_device_register(&pdev->dev,
+						       pdev->dev.of_node,
+						       "ddrc", rddrc,
+						       &ddrc_cooling_ops);
+	if (IS_ERR(cdev)) {
+		dev_err(&pdev->dev,
+			"failed to register thermal cooling device");
+		return PTR_ERR(cdev);
+	}
 
-	_rts_ddrc = rddr;
+	dev_info(&pdev->dev, "registe thermal cooling device success");
 
 	return 0;
 }
 
-static int rts_ddrc_remove(struct platform_device *pdev)
+static void rts_ddrc_remove(struct platform_device *pdev)
 {
-	thermal_event_unregister(&ddrc_nb);
-
 	sysfs_remove_group(&pdev->dev.kobj, &ddrc_attr_group);
-
-	_rts_ddrc = NULL;
-
-	return 0;
 }
 
 static struct platform_driver rts_ddrc_driver = {
@@ -286,8 +283,9 @@ static struct platform_driver rts_ddrc_driver = {
 		.of_match_table = rts_ddrc_dt_ids,
 	},
 	.probe = rts_ddrc_probe,
-	.remove = rts_ddrc_remove,
+	.remove_new = rts_ddrc_remove,
 };
+
 module_platform_driver(rts_ddrc_driver);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("GPL v2");
