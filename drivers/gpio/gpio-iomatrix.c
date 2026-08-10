1aaf445354573318634c61486b265618f6b306fc
quntao_liao <quntao_liao@realsil.com.cn>
2026-08-10 16:52:43 +0800

    feat(gpio): add RTS591x GPIO interrupt support
    Add nested interrupt support for fixed GPIO21 GPIO41 and GPIO42
    sources. Defer sleepable EC register updates to the IRQ bus sync
    callback and gate parent IRQs until configuration completes.
    
    Signed-off-by: quntao_liao <quntao_liao@realsil.com.cn>

--- drivers/gpio/gpio-iomatrix.c
+++ drivers/gpio/gpio-iomatrix.c
@@ -21,15 +21,19 @@
  * even if advised of the possibility of such damage.
  */
 
+#include <linux/bitfield.h>
 #include <linux/bits.h>
-#include <linux/dma-mapping.h>
 #include <linux/gpio/driver.h>
 #include <linux/i2c.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
 #include <linux/mfd/iomatrix.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
+#include <linux/seq_file.h>
 
 #define GPIO_DIR_MASK BIT(0)
 #define GPIO_DIR_IN   0
@@ -77,12 +81,11 @@
 #define GPIO_OUTCTRL_HIGH BIT(17)
 
 #define GPIO_INTCTRL_MASK      GENMASK(26, 24)
-#define GPIO_INTCTRL_SHIFT     24
-#define GPIO_INT_POSITIVE_EDGE (0 << GPIO_INTCTRL_SHIFT)
-#define GPIO_INT_NEGATIVE_EDGE (1 << GPIO_INTCTRL_SHIFT)
-#define GPIO_INT_DUAL_EDGE     (2 << GPIO_INTCTRL_SHIFT)
-#define GPIO_INT_LOW_LEVEL     (3 << GPIO_INTCTRL_SHIFT)
-#define GPIO_INT_HIGH_LEVEL    (4 << GPIO_INTCTRL_SHIFT)
+#define GPIO_INT_POSITIVE_EDGE FIELD_PREP(GPIO_INTCTRL_MASK, 0)
+#define GPIO_INT_NEGATIVE_EDGE FIELD_PREP(GPIO_INTCTRL_MASK, 1)
+#define GPIO_INT_DUAL_EDGE     FIELD_PREP(GPIO_INTCTRL_MASK, 2)
+#define GPIO_INT_LOW_LEVEL     FIELD_PREP(GPIO_INTCTRL_MASK, 3)
+#define GPIO_INT_HIGH_LEVEL    FIELD_PREP(GPIO_INTCTRL_MASK, 4)
 
 #define GPIO_INTEN_MASK	   BIT(28)
 #define GPIO_INTEN_DISABLE 0
@@ -92,17 +95,33 @@
 
 #define GPIO_GET_BIT(reg, mask) (((reg) & (mask)) ? 1 : 0)
 
-#define DRIVER_NAME "rts591x-gpio"
+#define DRIVER_NAME	       "rts591x-gpio"
+#define RTS591X_GPIO_IRQ_COUNT 3
+
+struct rts591x_gpio_irq_entry {
+	unsigned int pin;
+	unsigned int irq;
+};
 
 struct rts591x_gpio_chip {
 	struct regmap *regmap;
 	phys_addr_t base;
 	struct gpio_chip gc;
 	enum rts591x_model model;
+	const struct rts591x_gpio_irq_entry *irq_entries;
+	unsigned int num_irq_entries;
+	int virq[RTS591X_GPIO_IRQ_COUNT];
+	unsigned int irq_type[RTS591X_GPIO_IRQ_COUNT];
+	bool irq_configured;
+	bool irq_enabled[RTS591X_GPIO_IRQ_COUNT];
+	bool parent_enabled[RTS591X_GPIO_IRQ_COUNT];
+	bool irq_update[RTS591X_GPIO_IRQ_COUNT];
+	struct mutex irq_lock;
 };
 
-static const unsigned int gpio_pins[] = { 13,  16,  40,	 87,  88, 89,
-					  102, 104, 105, 112, 117 };
+static const unsigned int gpio_pins[] = {
+	13, 16, 21, 40, 41, 42, 87, 88, 89, 102, 104, 105, 112, 117, 126, 127
+};
 
 static const unsigned int gpio_pins_hpm[] = {
 	0,   1,	  2,   3,   4,	 9,   13,  14,	15,  16,  17,  18,
@@ -278,6 +297,293 @@ static int rts591x_gpio_set_config(struct gpio_chip *gc, unsigned int offset,
 	}
 }
 
+#ifdef CONFIG_GPIOLIB_IRQCHIP
+static const struct rts591x_gpio_irq_entry rts591x_gpio_irq_entries[] = {
+	{ 21, RTS591X_GPIO21_INT },
+	{ 41, RTS591X_GPIO41_INT },
+	{ 42, RTS591X_GPIO42_INT },
+};
+
+static int rts591x_gpio_irq_index(struct rts591x_gpio_chip *chip,
+				  unsigned int offset)
+{
+	int i;
+
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		if (chip->irq_entries[i].pin == offset)
+			return i;
+	}
+
+	return -EINVAL;
+}
+
+static void rts591x_gpio_irq_init_valid_mask(struct gpio_chip *gc,
+					     unsigned long *valid_mask,
+					     unsigned int ngpios)
+{
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+	int i;
+
+	bitmap_zero(valid_mask, ngpios);
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		unsigned int pin = chip->irq_entries[i].pin;
+
+		if (pin < ngpios)
+			set_bit(pin, valid_mask);
+	}
+}
+
+static void rts591x_gpio_irq_mask(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+	irq_hw_number_t hwirq = irqd_to_hwirq(d);
+	int index;
+
+	index = rts591x_gpio_irq_index(chip, hwirq);
+	if (index < 0)
+		return;
+
+	chip->irq_enabled[index] = false;
+	chip->irq_update[index] = true;
+	gpiochip_disable_irq(gc, hwirq);
+}
+
+static void rts591x_gpio_irq_unmask(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+	irq_hw_number_t hwirq = irqd_to_hwirq(d);
+	int index;
+
+	index = rts591x_gpio_irq_index(chip, hwirq);
+	if (index < 0)
+		return;
+
+	gpiochip_enable_irq(gc, hwirq);
+	chip->irq_enabled[index] = true;
+	chip->irq_update[index] = true;
+}
+
+static void rts591x_gpio_irq_ack(struct irq_data *d)
+{
+}
+
+static void rts591x_gpio_irq_bus_lock(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+
+	mutex_lock(&chip->irq_lock);
+}
+
+static void rts591x_gpio_irq_bus_sync_unlock(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+	int ret;
+	int i;
+
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		unsigned int pin = chip->irq_entries[i].pin;
+		unsigned int type = chip->irq_type[i];
+		unsigned int val;
+
+		if (!chip->irq_update[i])
+			continue;
+
+		switch (type & IRQ_TYPE_SENSE_MASK) {
+		case IRQ_TYPE_EDGE_FALLING:
+			val = GPIO_INT_NEGATIVE_EDGE;
+			break;
+		case IRQ_TYPE_EDGE_BOTH:
+			val = GPIO_INT_DUAL_EDGE;
+			break;
+		case IRQ_TYPE_LEVEL_LOW:
+			val = GPIO_INT_LOW_LEVEL;
+			break;
+		case IRQ_TYPE_LEVEL_HIGH:
+			val = GPIO_INT_HIGH_LEVEL;
+			break;
+		case IRQ_TYPE_EDGE_RISING:
+		default:
+			val = GPIO_INT_POSITIVE_EDGE;
+			break;
+		}
+
+		val |= GPIO_INTSTS_MASK;
+		if (chip->irq_enabled[i])
+			val |= GPIO_INTEN_ENABLE;
+
+		if (!chip->irq_enabled[i] && chip->parent_enabled[i]) {
+			disable_irq(chip->virq[i]);
+			chip->parent_enabled[i] = false;
+		}
+
+		ret = regmap_update_bits(chip->regmap, chip->base + pin * 4,
+					 GPIO_DIR_MASK | GPIO_INTCTRL_MASK |
+						 GPIO_INTSTS_MASK |
+						 GPIO_INTEN_MASK,
+					 GPIO_DIR_IN | val);
+		if (ret) {
+			dev_err(gc->parent, "failed to update GPIO%u IRQ: %d\n",
+				pin, ret);
+			continue;
+		}
+
+		if (chip->irq_enabled[i] && !chip->parent_enabled[i]) {
+			enable_irq(chip->virq[i]);
+			chip->parent_enabled[i] = true;
+		}
+
+		chip->irq_update[i] = false;
+	}
+
+	mutex_unlock(&chip->irq_lock);
+}
+
+static int rts591x_gpio_irq_set_type(struct irq_data *d, unsigned int type)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct rts591x_gpio_chip *chip = gpiochip_get_data(gc);
+	irq_hw_number_t hwirq = irqd_to_hwirq(d);
+	int index;
+
+	index = rts591x_gpio_irq_index(chip, hwirq);
+	if (index < 0)
+		return index;
+
+	switch (type & IRQ_TYPE_SENSE_MASK) {
+	case IRQ_TYPE_EDGE_RISING:
+	case IRQ_TYPE_EDGE_FALLING:
+	case IRQ_TYPE_EDGE_BOTH:
+	case IRQ_TYPE_LEVEL_LOW:
+	case IRQ_TYPE_LEVEL_HIGH:
+		chip->irq_type[index] = type;
+		chip->irq_update[index] = true;
+		return 0;
+	default:
+		return -EINVAL;
+	}
+}
+
+static void rts591x_gpio_irq_print_chip(struct irq_data *data,
+					struct seq_file *p)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(data);
+
+	seq_puts(p, dev_name(gc->parent));
+}
+
+static const struct irq_chip rts591x_gpio_irq_chip = {
+	.irq_ack = rts591x_gpio_irq_ack,
+	.irq_mask = rts591x_gpio_irq_mask,
+	.irq_unmask = rts591x_gpio_irq_unmask,
+	.irq_set_type = rts591x_gpio_irq_set_type,
+	.irq_bus_lock = rts591x_gpio_irq_bus_lock,
+	.irq_bus_sync_unlock = rts591x_gpio_irq_bus_sync_unlock,
+	.irq_print_chip = rts591x_gpio_irq_print_chip,
+	.flags = IRQCHIP_IMMUTABLE,
+	GPIOCHIP_IRQ_RESOURCE_HELPERS,
+};
+
+static irqreturn_t rts591x_gpio_irq_handler(int irq, void *data)
+{
+	struct rts591x_gpio_chip *chip = data;
+	int i;
+
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		if (chip->virq[i] == irq) {
+			unsigned int pin = chip->irq_entries[i].pin;
+			int nested_irq;
+
+			if (!chip->parent_enabled[i])
+				return IRQ_HANDLED;
+
+			nested_irq = irq_find_mapping(chip->gc.irq.domain, pin);
+			if (nested_irq <= 0)
+				return IRQ_NONE;
+
+			handle_nested_irq(nested_irq);
+			return IRQ_HANDLED;
+		}
+	}
+
+	return IRQ_NONE;
+}
+
+static int rts591x_gpio_irq_setup(struct platform_device *pdev,
+				  struct rts591x_gpio_chip *chip)
+{
+	struct gpio_irq_chip *girq = &chip->gc.irq;
+	int i;
+
+	if (chip->model != MODEL_ESCM)
+		return 0;
+
+	chip->irq_configured = true;
+	mutex_init(&chip->irq_lock);
+	chip->irq_entries = rts591x_gpio_irq_entries;
+	chip->num_irq_entries = ARRAY_SIZE(rts591x_gpio_irq_entries);
+
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		chip->virq[i] = platform_get_irq(pdev, i);
+		if (chip->virq[i] < 0)
+			return chip->virq[i];
+		irq_set_status_flags(chip->virq[i], IRQ_NOAUTOEN);
+	}
+
+	gpio_irq_chip_set_chip(girq, &rts591x_gpio_irq_chip);
+	girq->parent_handler = NULL;
+	girq->num_parents = 0;
+	girq->parents = NULL;
+	girq->default_type = IRQ_TYPE_NONE;
+	girq->handler = handle_simple_irq;
+	girq->init_valid_mask = rts591x_gpio_irq_init_valid_mask;
+	girq->threaded = true;
+
+	return 0;
+}
+
+static int rts591x_gpio_irq_request_parents(struct platform_device *pdev,
+					    struct rts591x_gpio_chip *chip)
+{
+	struct device *dev = &pdev->dev;
+	int ret;
+	int i;
+
+	if (chip->model != MODEL_ESCM || !chip->irq_configured)
+		return 0;
+
+	for (i = 0; i < chip->num_irq_entries; i++) {
+		ret = devm_request_threaded_irq(dev, chip->virq[i], NULL,
+						rts591x_gpio_irq_handler,
+						IRQF_ONESHOT, dev_name(dev),
+						chip);
+		if (ret)
+			return dev_err_probe(
+				dev, ret,
+				"failed to request GPIO%u parent IRQ\n",
+				chip->irq_entries[i].pin);
+	}
+
+	return 0;
+}
+
+#else
+static int rts591x_gpio_irq_setup(struct platform_device *pdev,
+				  struct rts591x_gpio_chip *chip)
+{
+	return 0;
+}
+
+static int rts591x_gpio_irq_request_parents(struct platform_device *pdev,
+					    struct rts591x_gpio_chip *chip)
+{
+	return 0;
+}
+#endif /* CONFIG_GPIOLIB_IRQCHIP */
+
 static int rts591x_gpio_probe(struct platform_device *pdev)
 {
 	struct rts591x_gpio_chip *chip;
@@ -313,6 +619,7 @@ static int rts591x_gpio_probe(struct platform_device *pdev)
 	chip->gc.label = dev_name(dev);
 	chip->gc.parent = dev;
 	chip->gc.owner = THIS_MODULE;
+	chip->gc.can_sleep = true;
 
 	chip->gc.request = rts591x_gpio_request;
 	chip->gc.direction_input = rts591x_gpio_direction_input;
@@ -323,9 +630,15 @@ static int rts591x_gpio_probe(struct platform_device *pdev)
 	chip->gc.set_config = rts591x_gpio_set_config;
 	chip->gc.init_valid_mask = rts591x_gpio_init_valid_mask;
 
-	ret = devm_gpiochip_add_data(dev, &chip->gc, chip);
+	ret = rts591x_gpio_irq_setup(pdev, chip);
+	if (ret)
+		return ret;
 
-	return ret;
+	ret = devm_gpiochip_add_data(dev, &chip->gc, chip);
+	if (ret)
+		return ret;
+
+	return rts591x_gpio_irq_request_parents(pdev, chip);
 }
 
 static const struct of_device_id rts591x_gpio_of_match[] = {
